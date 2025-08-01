ea096a7ce112e1c36d56bb198b496df852c31dd4
Liana Perry <62174756+lperry022@users.noreply.github.com>
2025-08-01 16:09:02 +1000

    fixed mouse glow

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
 import './globals.css';
+import MouseGlow from '@/components/MouseGlow';
 
 export const metadata = {
   title: 'Liana Perry'
 };
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MouseGlow />
+        {children}
+      </body>
     </html>
   );
 }
