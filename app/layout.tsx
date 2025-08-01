9a2125726c64421ea6013938f77b03176d68d215
Liana Perry <62174756+lperry022@users.noreply.github.com>
2025-08-01 15:08:48 +1000

    Re-added layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Liana Perry'
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
