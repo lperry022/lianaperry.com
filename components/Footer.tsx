7a35be04c6a7e63062a61a1e09530c745d591b62
Liana Perry <62174756+lperry022@users.noreply.github.com>
2025-08-17 16:55:28 +1000

    clean up footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,9 +10,6 @@ const Footer = () => {
         &copy; {new Date().getFullYear()} Liana Perry. All rights reserved.
       </div>
       <div className="space-x-4">
-        <Link href="/maintenance" className="hover:text-purple-300 transition">Privacy</Link>
-        <Link href="/maintenance" className="hover:text-purple-300 transition">Terms</Link>
-        <Link href="/contact" className="hover:text-purple-300 transition">Contact</Link>
       </div>
     </footer>
   );
